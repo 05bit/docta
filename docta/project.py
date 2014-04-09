1dc6e8177de89533fb186f207395e9636164bc19
Alexey Kinyov <rudy@05bit.com>
2014-04-10 00:08:09 +0400

    HTML renderer performing structure rendering

--- docta/project.py
+++ docta/project.py
@@ -4,35 +4,21 @@ Docta projects handler.
 from __future__ import absolute_import, division, print_function, unicode_literals
 from future.builtins import *
 import os
+import shutil
 import docta.exceptions
+import docta.render
+import docta.fs as fs
 
 # Defaults
 INDEX_FILE = 'index.md'
-OUT_FORMAT = 'html'
-
-# Shortcuts
-os_join = os.path.join
-os_real = os.path.realpath
-os_dir = os.path.dirname
-os_sep = os.path.sep
-os_mkdirs = lambda p: os.makedirs(p, mode=0755)
-os_mkdirs_noerr = docta.exceptions.suppress(OSError, IOError)(os_mkdirs)
-
-
-def path_for_file(base, relative):
-    """Get real directory path by base dir and relative file path."""
-    return os_real(os_dir(os_join(base, relative)))
-
-
-def path_for_dir(base, relative):
-    """Get real directory path by base dir and relative dir path."""
-    return os_real(os_join(base, relative))
+OUT_FORMAT_DEFAULT = 'html'
+OUT_RESOURCES_DIR = 'static'
 
 
 class Project(object):
     """
     Project data handler: scans project directories, manages resources,
-    creates output directories, creates empty project, builds project.
+    creates empty project, builds project.
     """
     def __init__(self, path, **config):
         self.path = path
@@ -57,7 +43,7 @@ class Project(object):
         for path, subdirs, files in os.walk(input_dir):
             to_render = self.files_to_render(files)
             if to_render:
-                rel_path = path.replace(input_dir, '', 1).strip(os_sep)
+                rel_path = path.replace(input_dir, '', 1).strip(fs.sep)
                 if not self.is_relpath_masked(rel_path):
                     self.input_tree.append((rel_path, to_render))
 
@@ -67,21 +53,10 @@ class Project(object):
         """
         self.load()
 
-        input_dir = self.input_dir()
-        for out_format in (formats or [OUT_FORMAT]):
-            # TODO: here we should pick a renderer for format!
-            #       But now we'll just perform raw render.
-            output_dir = self.output_dir(out_format)
-            os_mkdirs_noerr(output_dir)
-
-            output_tree = []
-            for rel_path, files in self.input_tree:
-                os_mkdirs_noerr(path_for_dir(output_dir, rel_path))
-
-                for name in files:
-                    in_file_path = os_join(input_dir, rel_path, name)
-                    out_file_path = os_join(output_dir, rel_path, name)
-                    print('%s => %s' % (in_file_path, out_file_path))
+        for out_format in (formats or [OUT_FORMAT_DEFAULT]):
+            render_class = docta.render.get_renderer(out_format)
+            renderer = render_class(self)
+            renderer.render()
 
     def init(self):
         """
@@ -93,13 +68,13 @@ class Project(object):
         """
         Output directory for specified format.
         """
-        return path_for_dir(self.path, self.config['output'][out_format])
+        return fs.path_for_dir(self.path, self.config['output'][out_format])
 
     def input_dir(self):
         """
         Input dir based on project index file.
         """
-        return path_for_file(self.path, self.config.get('index', INDEX_FILE))
+        return fs.path_for_file(self.path, self.config.get('index', INDEX_FILE))
 
     def files_to_render(self, files):
         """
@@ -115,13 +90,13 @@ class Project(object):
         """
         Create output directory if doesn't exist.
         """
-        os_mkdirs_noerr(self.output_dir(out_format))
+        fs.mkdirs_noerr(self.output_dir(out_format))
 
     def is_relpath_masked(self, relative):
         """
         Checks if relative dir path is masked (not rendered) in build.
         """
-        for name in relative.strip(os_sep).split(os_sep):
+        for name in relative.strip(fs.sep).split(fs.sep):
             if self.is_name_masked(name):
                 return True
         return False
@@ -145,4 +120,6 @@ class Project(object):
         """
         Copy resources to output directory.
         """
-        raise Exception(NotImplemented)
+        in_resources = fs.path_for_dir(self.path, self.config.get('resources', '_resources'))
+        out_resources = fs.path_for_dir(self.output_dir(out_format), OUT_RESOURCES_DIR)
+        shutil.copytree(in_resources, out_resources)
--- docta/render.py
+++ docta/render.py
@@ -1,3 +1,15 @@
 """
 Rendering tools for Docta.
 """
+import importlib
+
+
+def get_renderer(out_format, **custom):
+    """
+    Get renderer class for specified format.
+    """
+    module_name = custom.get(out_format, 'docta.renderers.%s' % out_format)
+    render_module = importlib.import_module(module_name)
+    render_class = render_module.Renderer
+    render_class.out_format = out_format
+    return render_class
