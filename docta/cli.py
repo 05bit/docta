90a4966cfe58524945b66dd12b67252b2705a809
Alexey Kinyov <rudy@05bit.com>
2014-04-09 12:47:27 +0400

    basic project structure and setup.py

---
+++ docta/cli.py
@@ -0,0 +1,14 @@
+"""
+Command line interface (CLI) for Docta.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from future.builtins import *
+
+
+def main():
+    print("Hello, I'm Docta!")
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+Docta is ...
+"""
+from setuptools import setup, find_packages
+
+__version__ = '0.0.1'
+
+setup(
+    name='Docta',
+    version=str(__version__),
+    author='Alexey Kinyov',
+    author_email='rudy@05bit.com',
+    url='https://github.com/05bit/python-docta',
+    description='A new documentation kit.',
+    long_description=__doc__,
+    license='BSD',
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'future',
+        'staticjinja',
+        'misaka',
+        'watchdog',
+        'docutils'
+    ],
+    entry_points = {
+        'console_scripts': 'docta = docta.cli:main'
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet',
+        'Topic :: Text Processing',
+        'Topic :: Utilities'
+    ],
+    use_2to3=True
+)
