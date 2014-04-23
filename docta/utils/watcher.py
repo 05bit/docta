2bb80ced6cddcf66f7c82fc1ad948d73d5afb049
Alexey Kinyov <rudy@05bit.com>
2014-04-24 00:26:46 +0400

    smarter watcher + default styles tweaks

--- docta/utils/watcher.py
+++ docta/utils/watcher.py
@@ -3,6 +3,7 @@ Watch & rebuild project utils.
 """
 from __future__ import absolute_import, print_function, unicode_literals
 from future.builtins import super
+import os
 import sys
 import time
 import threading
@@ -21,36 +22,85 @@ def watch(project):
     Start watching project input directory to perform automatic builds on changes.
     It doesn't lock thread. Returns `watchdog.observers.Observer` instance.
     """
-    log.message("Watching directory: %s" % project.input_dir())
-    observer = watchdog.observers.Observer()
-    observer.schedule(ProjectEventHandler(project),
-                      project.input_dir(),
-                      recursive=True)
+    observer = ProjectObserver(project)
     observer.start()
     return observer
 
 
-class ProjectEventHandler(events.FileSystemEventHandler):
+class ProjectObserver(watchdog.observers.Observer):
     """
-    Watches events in project input directory and automatically
-    builds it.
+    Watches changes in project base directory and automatically
+    rebuilds project.
     """
     def __init__(self, project, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # setup
         self.project = project
-        self.config = self.project.config.copy()
         self.rebuild_timer = None
         self.rebuild_going = False
+
+    def start(self, *args, **kwargs):
+        """
+        Schedule all handlers and start watching thread.
+        """
+        self.build(initial=True)
+        self.schedule_all()
+        super().start(*args, **kwargs)
+
+    def schedule_all(self):
+        """
+        Schedule all project directories handlers.
+        """
+        log.message("Watching directory: %s" % self.project.input_dir())
+        self.schedule(ProjectPathEventHandler(self),
+                      self.project.input_dir(),
+                      recursive=True)
+
+    def needs_rebuild(self):
+        """
+        Re(schedule) project rebuild after some delay (500 ms by default). The reason of
+        delay is we want to make sure all buch of recent events have been dispatched.
+        """
+        if self.rebuild_timer:
+            self.rebuild_timer.cancel()
+
+        self.rebuild_timer = threading.Timer(REBUILD_DELAY, self.build)
+        self.rebuild_timer.start()
+
+    def build(self, initial=False):
+        """
+        Build project and log time spent for operation.
+        """
+        if not self.rebuild_going:
+            self.rebuild_going = True
+            t0 = time.time()
+
+            if not initial:
+                self.unschedule_all()
+            
+            self.project.build(['html'])
+
+            if not initial:
+                self.schedule_all()
+
+            t1 = time.time()
+            self.rebuild_going = False
+            log.success("  %s" % "rebuild time: %.2fms" % (1000.0 * (t1 - t0)))
+
+
+class ProjectPathEventHandler(events.FileSystemEventHandler):
+    """
+    Rebuilds project on input project data changes.
+    """
+    def __init__(self, observer, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # setup
+        self.observer = observer
+        self.project = observer.project
+        self.config = self.project.config.copy()
         self.events_file = (events.FileModifiedEvent, events.FileCreatedEvent, events.FileDeletedEvent)
         self.events_dirs = (events.DirMovedEvent, events.DirDeletedEvent)
-        self.ignore_dirs = [self.project.output_dir('html')]
-        log.message("Watching ignore: %s" % self.ignore_dirs)
         log.message("Watching patterns: %s" % ' '.join(self.get_patterns()))
-
-        # initial build
-        self.build()
 
     def get_patterns(self):
         """
@@ -61,53 +111,15 @@ class ProjectEventHandler(events.FileSystemEventHandler):
             .setdefault('server', {})
             .setdefault('watch', WATCH_PATTERNS_DEFAULT))
 
-    def is_path_ignored(self, path):
-        """
-        Check if path is within ignored dirs.
-        """
-        return any([fs.issub(path, base) for base in self.ignore_dirs])
-
     def on_any_event(self, event):
         """
         Filter file/dir events and schedule project rebuild.
         """
-        if not self.is_path_ignored(event.src_path):
-            if isinstance(event, self.events_file):
-                name = fs.basename(event.src_path)
-                if any([fs.match(name, mask) for mask in self.get_patterns()]):
-                    self.log_event("file updated: %s" % event.src_path)
-                    self.schedule_build()
-            elif isinstance(event, self.events_dirs):
-                self.log_event("directory updated: %s" % event.src_path)
-                self.schedule_build()
-
-    def schedule_build(self):
-        """
-        Re(schedule) project build after some delay (500 ms by default). We want
-        to make sure all buch of recent events have been dispatched.
-        """
-        # log.message("Schedule project rebuild")
-        if self.rebuild_timer:
-            self.rebuild_timer.cancel()
-        self.rebuild_timer = threading.Timer(REBUILD_DELAY, self.build)
-        self.rebuild_timer.start()
-
-    def build(self):
-        """
-        Build project and log time spent for operation.
-        """
-        t0 = time.time()
-        if self.rebuild_going:
-            self.schedule_build()
-        else:
-            self.rebuild_going = True
-            self.project.build(['html'])
-            t1 = time.time()
-            self.rebuild_going = False
-        self.log_event("rebuild time: %.2fms" % (1000.0 * (t1 - t0)))
-
-    def log_event(self, text):
-        """
-        Shortcut for nicely formatted event logging.
-        """
-        log.success("  %s" % text)
+        if isinstance(event, self.events_file):
+            name = fs.basename(event.src_path)
+            if any([fs.match(name, mask) for mask in self.get_patterns()]):
+                log.success("  %s" % "file updated: %s" % event.src_path)
+                self.observer.needs_rebuild()
+        elif isinstance(event, self.events_dirs):
+            log.success("  %s" % "directory updated: %s" % event.src_path)
+            self.observer.needs_rebuild()
