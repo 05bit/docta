525c961acc684fed35db81a8373b29a71f51d828
Alexey Kinyov <rudy@05bit.com>
2014-04-29 00:48:50 +0400

    bump version to 0.2a

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://docta.05bit.com
 """
 from setuptools import setup, find_packages
 
-__version__ = '0.1'
+__version__ = '0.2a'
 
 install_requires=[
     'future',
@@ -26,12 +26,12 @@ except ImportError:
     install_requires.append('argparse')
 
 setup(
-    name='Docta',
+    name="Docta",
     version=str(__version__),
-    author='Alexey Kinyov',
+    author="Alexey Kinyov",
     author_email='rudy@05bit.com',
     url='https://github.com/05bit/python-docta',
-    description='Docta is a new healing docs kit.',
+    description="Docta is a new healing docs kit.",
     long_description=__doc__,
     license='BSD',
     zip_safe=False,
@@ -42,7 +42,7 @@ setup(
         'console_scripts': 'docta = docta.cli:main'
     },
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
